ee72051f56490fe9d029cd5f98e92b4b6cfac7ac
andyerdene <andyerdene@gmail.com>
2024-01-08 11:43:44 +0800

    project init

---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+const Card = () => {
+  return (
+    <div>
+      <p>Hello Card</p>
+    </div>
+  );
+};
+export default Card;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import App from "./App";
+import ReactDom from "react-dom";
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<App />);
